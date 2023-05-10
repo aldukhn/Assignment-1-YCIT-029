1b15149e201d56764550f6d9f4f759248d3d7013
Abdul Omar <abbcc123123@gmail.com>
2023-05-09 20:28:04 -0400

    Add the Expiry Date of the token

--- generateToken.js
+++ generateToken.js
@@ -6,11 +6,15 @@ dotenv.config();
 const SECRET_KEY = process.env.SECRET_KEY;
 
 export function generateToken(userId) {
-  const payload = {
+  var payload = {
     userId,
     iat: Math.floor(Date.now() / 1000),
-    exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7,
+    exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30,
   };
 
-  return jwt.sign(payload, SECRET_KEY);
+  let key = jwt.sign(payload, SECRET_KEY);
+  let decoded = jwt.decode(key);
+  console.log("Token will expire at: ", new Date(decoded.exp * 1000));
+
+  return key;
 }
